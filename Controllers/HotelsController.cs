bed91cb52056a7d3db86b56910e1ec7abd6d63e1
CEEJ987 <cvsanders87@gmail.com>
2023-08-04 12:22:58 -0500

    Add project files.

---
+++ Controllers/HotelsController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Lab12.Data;
+using Lab12.Models;
+using Lab12.Models.Interfaces;
+using Lab12.Models.Services;
+using System.Runtime.InteropServices;
+//using System.Web.Http;
+
+namespace Lab12.Controllers
+{
+    /*
+     * app.get("/", getData);
+     * app.post("/", updloadData);
+     */
+    //https://localhost:1234/api/Hotels/
+    //https://asyncinn.com/api/Hotels
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelsController : ControllerBase
+    {
+        private readonly AsyncInnContext _context;
+        private readonly IHotel _hotel;
+
+        public HotelsController(AsyncInnContext context, IHotel hotel)
+        {
+            _context = context;
+            _hotel = hotel;
+
+        }
+
+        // GET: api/Hotels
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Hotel>>> GetHotel()
+        {
+            return await _hotel.GetHotel();
+        }
+
+        // GET: api/Hotels/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Hotel>> GetHotel(int id)
+        {
+            return await _hotel.GetHotel(id);
+        }
+
+        // PUT: api/Hotels/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutHotel(int id, Hotel hotel)
+        {
+            if (id != hotel.ID)
+            {
+                return BadRequest();
+            }
+            await _hotel.PutHotel(id, hotel);
+
+            return NoContent();
+        }
+
+        // POST: api/Hotels
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Hotel>> PostHotel(Hotel hotel)
+        {
+
+            await _hotel.PostHotel(hotel);
+
+            return CreatedAtAction("GetHotel", new { id = hotel.ID }, hotel);
+        }
+
+        // DELETE: api/Hotels/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteHotel(int id)
+        {
+            await _hotel.DeleteHotel(id);
+            return NoContent();
+        }
+
+        private bool HotelExists(int id)
+        {
+            return _hotel.HotelExists(id);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+using Lab12.Data;
+using Lab12.Models.Interfaces;
+using Lab12.Models.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+
+namespace Lab12
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            /* TODO
+            builder.Services.addContext
+             */
+            builder.Services.AddDbContext<AsyncInnContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration
+                    .GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddTransient<IHotel, HotelService>();
+
+            var app = builder.Build();
+
+            //app.MapGet("/", () => "Hello World!");
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            //https://localhost:44391/Hotel/CheckIn/
+            //https://website/Hotel/CheckOut
+            //https://website/Hotel/
+
+            /*
+             * 
+             * 
+            
+             */
+
+            app.Run();
+        }
+    }
+}
